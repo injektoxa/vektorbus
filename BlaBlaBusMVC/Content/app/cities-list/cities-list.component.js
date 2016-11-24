1c56815cc549fda855e27b9deeaa61989d788e2e
NIX\kovalenko.v <dmitriy.kovalenko@nixsolutions.com>
2016-11-24 11:26:06 +0200

    Implemented adding new city functionality

--- BlaBlaBusMVC/Content/app/cities-list/cities-list.component.js
+++ BlaBlaBusMVC/Content/app/cities-list/cities-list.component.js
@@ -5,6 +5,24 @@ angular.
   component('citiesList', {
       templateUrl: 'cities-list/cities-list.template.html',
       controller: ['City', function CitiesListController(City) {
+          var that = this;
+
           this.cities = City.query();
+          this.showAddCityForm = false;
+          this.city = {};
+
+          this.showAddForm = function () {
+              that.showAddCityForm = !that.showAddCityForm;
+          }
+
+          this.saveCity = function (city) {
+              let thatCity = city;
+              City.add(city);
+              that.showAddCityForm = false;
+              setTimeout(function () {
+                  that.cities = City.query();
+                  thatCity = {};
+              }, 100);
+          }
       }]
   });
