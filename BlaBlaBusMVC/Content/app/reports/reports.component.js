eca5d40c4b660a58b0dca155f7e204644977c3ea
Ruslan Borovok <borovok.ruslan@outlook.com>
2016-12-02 17:44:11 +0200

    created reports page

---
+++ BlaBlaBusMVC/Content/app/reports/reports.component.js
@@ -0,0 +1,56 @@
+﻿'use strict';
+
+// Register `clientList` component, along with its associated controller and template
+angular.module('reports')
+    .component('reports',
+    {
+    	templateUrl: 'reports/reports.template.html',
+        controller: [
+            'Agent', 'Report', '$filter',
+            function (Agent, Report, $filter) {
+            	var that = this;
+		
+            	that.agents = Agent.query();
+            	that.agent = {};
+                that.dateTimeFormat = "dd/MM/yyyy";
+            	that.dateFrom = new Date();
+            	that.dateTo = new Date();
+            	that.dateOptions = {
+            		showWeeks: false,
+            		startingDay: 0,
+            		maxDate: new Date()
+            	}
+            	that.reports = [];
+                that.reportTitle = '';
+                that.totalTitle = '';
+                that.totalPrice = '';
+
+                that.onGetReports = function(reports) {
+                    that.reports = reports;
+                    that.reportTitle = 'Отчет по агенту ' + that.agent.FullName;
+                    that.totalTitle = 'Итого за период с ' +
+                        $filter('date')(that.dateFrom, "yyyy-MM-dd") +
+                        ' по ' +
+                        $filter('date')(that.dateTo, "yyyy-MM-dd") +
+                        ': ';
+                    that.totalPrice = reports.reduce(function(previousValue, currentValue) {
+                        return previousValue.AgentCompensation + currentValue.AgentCompensation;
+                    });
+                };
+
+                that.createReport = function () {
+                    that.reportTitle = '';
+                    that.totalTitle = '';
+                    that.totalPrice = '';
+
+                    var options = {
+                        id: that.agent.Id,
+                        dateFrom: $filter('date')(that.dateFrom, "yyyy-MM-dd"),
+                        dateTo: $filter('date')(that.dateTo, "yyyy-MM-dd")
+                    };
+
+                    Report.query(options, that.onGetReports);
+                };
+            }
+        ]
+    });
