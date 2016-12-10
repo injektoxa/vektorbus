357bddc06a0b1f8fdd465f9349e19061bfa58df6
Ruslan Borovok <borovok.ruslan@outlook.com>
2016-12-10 11:31:44 +0200

    created pdfMaker service

---
+++ BlaBlaBusMVC/Content/app/pdfMaker/pdfMaker.service.js
@@ -0,0 +1,15 @@
+﻿'use strict';
+
+// Register `pdfMaker` component, along with its associated controller and template
+angular.
+    module('pdfMaker').
+    factory('PdfMaker', function() {
+        return {
+            createAndDownload: function(options) {
+                pdfMake.createPdf(options.docDefinition).download();
+            },
+            createAndOpen: function(options) {
+                pdfMake.createPdf(options.docDefinition).open();
+            }
+        }
+    });
