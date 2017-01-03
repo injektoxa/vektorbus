2afe240d31722e34e6636e97ed2f2f204bf2c917
Dmitriy Kovalenko <dmitriy.kovalenko@nixsolutions.com>
2017-01-03 11:44:00 +0200

    Implemented displaying trip direction via google maps api.

--- BlaBlaBusMVC/Content/app/trip-list/trip-list.module.js
+++ BlaBlaBusMVC/Content/app/trip-list/trip-list.module.js
@@ -1,3 +1,3 @@
 ﻿'use strict';
 
-angular.module('tripList', ['core.trip','core.bus']);
+angular.module('tripList', ['core.trip', 'core.bus', 'ngMap']);
--- BlaBlaBusMVC/Content/gulpfile.js
+++ BlaBlaBusMVC/Content/gulpfile.js
@@ -101,6 +101,7 @@ gulp.task('bower_components', function () {
                     'app/bower_components/underscore/underscore-min.js',
                     'app/bower_components/angular-local-storage/dist/angular-local-storage.min.js',
                     'app/bower_components/angular-bootstrap/ui-bootstrap.min.js',
+                    'app/bower_components/ngmap/build/scripts/ng-map.min.js'
         ], { base: 'app/' })
             .pipe(concat('scripts.min.js'))
             .pipe(gulp.dest('app'));
