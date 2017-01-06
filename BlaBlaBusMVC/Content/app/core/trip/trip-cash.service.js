75b7410afb858cb7aab7e0c02379b45575c53efc
Dmitriy Kovalenko <dmitriy.kovalenko@nixsolutions.com>
2017-01-06 12:34:20 +0200

    Adjusted logic of clculating incoms and expenses, added new columns to trip reports, implemented calculating of agent expenses

--- BlaBlaBusMVC/Content/app/core/trip/trip-cash.service.js
+++ BlaBlaBusMVC/Content/app/core/trip/trip-cash.service.js
@@ -1,15 +1,30 @@
-﻿angular.module('core.trip')
+﻿'use strict';
+
+const expenseSumFun = (acc, expense) => acc + expense.Cost;
+
+angular.module('core.trip')
     .factory('tripCashService', function () {
         return {
-            countDriverCashBox: function (trip) {
-                var expenseSumFun = (acc, expense) => acc + expense.Cost;
+            countIncomes: function (trip) {
+                var expenses = this.countTotalExpenses(trip);
+                var incomes = this.countDriverCashBox(trip);
 
+                return incomes - (expenses.compolsuryTotal + expenses.unexpectedTotal + expenses.agentExpensesTotal);
+            },
+            countDriverCashBox: function (trip) {
+                return trip.tripClients.reduce((acc, client) => acc + client.Price, 0);
+            },
+            countTotalExpenses: function (trip) {
                 var compulsoryExpensesSum = trip.compulsoryExpenses.reduce(expenseSumFun, 0);
                 var unexpectedExpensesSum = trip.unexpectedExpenses.reduce(expenseSumFun, 0);
 
-                var incomes = trip.tripClients.reduce((acc, client) => acc + client.Price, 0);
+                var agentExpensesSum = trip.tripClients.reduce((acc, client) => acc + client.AgentPrice, 0);
 
-                return incomes - (compulsoryExpensesSum + unexpectedExpensesSum);
+                return {
+                    compolsuryTotal: compulsoryExpensesSum,
+                    unexpectedTotal: unexpectedExpensesSum,
+                    agentExpensesTotal: agentExpensesSum
+                };
             }
         }
     });
